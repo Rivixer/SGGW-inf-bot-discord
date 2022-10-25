285e44be0bedfceeb9bcc1fcf8ee4b5f2bde2dcd
Wiktor <wijaweq@gmail.com>
2022-10-25 13:50:30 +0200

    Add is_bot_channel_or(...) to checks

--- utils/checks.py
+++ utils/checks.py
@@ -15,3 +15,19 @@ def is_bot_channel():
         bot_channel = ctx.guild.get_channel(settings.get('BOT_CHANNEL_ID'))
         return bot_channel == ctx.channel
     return commands.check(predicate)
+
+
+def is_bot_channel_or(setting_names):
+    def predicate(ctx: commands.Context):
+        bot_channel = ctx.guild.get_channel(settings.get('BOT_CHANNEL_ID'))
+        if bot_channel == ctx.channel:
+            return True
+
+        for setting_name in setting_names:
+            id = settings.get(setting_name)
+            if ctx.guild.get_channel(id) == ctx.channel:
+                return True
+
+        return False
+
+    return commands.check(predicate)
