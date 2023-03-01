6793e6cf2df37680904a3707fe57250685244693
Wiktor <wijaweq@gmail.com>
2023-03-01 20:16:02 +0100

    Refactor infomation

---
+++ sggwbot/errors.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: MIT
+"""A module containing all custom exceptions."""
+
+
+class SGGWBotError(Exception):
+    """Base exception for all SGGWBot exceptions."""
+
+
+class UpdateEmbedError(SGGWBotError):
+    """Cannot update the embed."""
+
+
+class ChangeMaxGroupsError(SGGWBotError):
+    """Changing max numbers of groups failed."""
+
+
+class RegistrationError(SGGWBotError):
+    """Registration failed."""
