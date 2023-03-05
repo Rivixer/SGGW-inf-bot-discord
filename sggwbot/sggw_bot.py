bb75245c3b9eb85b7c242fa2cd01cdb84cba2ae0
Wiktor <wijaweq@gmail.com>
2023-03-05 21:21:16 +0100

    Remove logging, add get_bot_channel function, add registration cog path

--- sggwbot/sggw_bot.py
+++ sggwbot/sggw_bot.py
@@ -16,8 +16,9 @@ Examples
         bot.add_cog(Cog(bot))
 """
 
+from __future__ import annotations
+
 import json
-import logging
 import os
 import sys
 import time
@@ -25,14 +26,13 @@ from pathlib import Path
 
 import dotenv
 import nextcord
+from nextcord.channel import TextChannel
 from nextcord.ext import commands
 from nextcord.flags import Intents
 from nextcord.guild import Guild
 
 from sggwbot.console import Console
 from sggwbot.utils import ProjectUtils
-
-_logger = logging.getLogger(__name__)
 
 
 class SGGWBot(commands.Bot):
@@ -46,9 +46,12 @@ class SGGWBot(commands.Bot):
     __slots__ = (
         "_prefix",
         "_guild_id",
+        "_bot_channel_id",
     )
 
     _guild_id: int
+    _prefix: str
+    _bot_channel_id: int
 
     def __init__(self) -> None:
         intents = Intents.all()
@@ -84,11 +87,10 @@ class SGGWBot(commands.Bot):
             with open(path, "w", encoding="utf-8") as f:
                 json.dump(model, f, indent=4)
 
-            _logger.critical(
-                "The '%s' file did not exist.\n"
+            Console.critical_error(
+                f"The '{path}' file did not exist.\n"
                 "A prototype has been created.\n"
                 "Complete it and start the bot again.",
-                path,
             )
 
         with open(path, "r", encoding="utf-8") as f:
@@ -96,29 +98,41 @@ class SGGWBot(commands.Bot):
 
         guild_id = data.get("GUILD_ID")
         if not isinstance(guild_id, int):
-            _logger.critical("GUILD_ID in '%s' must be int", path)
-            sys.exit()
+            Console.critical_error(f"GUILD_ID in '{path}' must be int")
 
         prefix = data.get("PREFIX")
         if not isinstance(prefix, str):
-            _logger.critical("PREFIX in '%s' must be str", path)
-            sys.exit()
+            Console.critical_error(f"PREFIX in {path} must be str")
+
+        bot_channel_id = data.get("BOT_CHANNEL_ID")
+        if not isinstance(bot_channel_id, int):
+            Console.critical_error(f"BOT_CHANNEL_ID in {path} must be int")
 
         self._guild_id = guild_id
         self._prefix = prefix
+        self._bot_channel_id = bot_channel_id
 
     def get_default_guild(self) -> Guild:
         """Returns the guild where the bot is used."""
         return self.get_guild(self._guild_id)  # type: ignore
 
+    def get_bot_channel(self) -> TextChannel:
+        """Returns the bot channel."""
+        guild = self.get_default_guild()
+        channel = guild.get_channel(self._bot_channel_id)
+        assert isinstance(channel, TextChannel)
+        return channel
+
     def _load_cogs(self) -> None:
+        # from .paths import Path
+
         paths = [
             "sggwbot.assigning_roles.py",
             "sggwbot.information.py",
             "sggwbot.project.py",
             "sggwbot.calendar.py",
             "sggwbot.status.py",
-            # "sggwbot.registration.py",
+            "sggwbot.registration.py",
             "sggwbot.messaging.py",
         ]
 
