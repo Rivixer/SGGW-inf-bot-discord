09b00aeeb82c14a88c1163b027b0db4c2dcc7c5c
Wiktor Jaworski <wijaweq@gmail.com>
2023-07-06 23:06:54 +0200

    Update to 0.7.2

--- sggwbot/__init__.py
+++ sggwbot/__init__.py
@@ -1,5 +1,25 @@
+"""
+SGGW-inf-bot-discord
+--------------------
+
+A Discord bot created to manage the Discord server for computer
+science students at the Warsaw University of Life Sciences.
+
+Features
+--------
+- role assigment based on reactions
+- event management, such as a test or exam
+- bot messaging, including embeds
+- registering users with the student's email address
+- setting bot's status
+- management of voice channels
+"""
+
 __title__ = "SGGW-inf-bot-discord"
 __author__ = "Wiktor Jaworski"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023 Wiktor Jaworski"
-__version__ = "0.7.1"
+__version__ = "0.7.2"
+
+from . import console, errors, utils
+from .sggw_bot import SGGWBot
