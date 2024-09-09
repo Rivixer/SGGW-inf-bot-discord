ae70b1709a459369062e6b10f6ed4d312fcb1a7b
Wiktor Jaworski <wijaweq@gmail.com>
2024-09-09 11:10:45 +0200

    Update to 0.9.0

--- sggwbot/__init__.py
+++ sggwbot/__init__.py
@@ -13,13 +13,14 @@ Features
 - registering users with the student's email address
 - setting bot's status
 - management of voice channels
+- custom plugins
 """
 
 __title__ = "SGGW-inf-bot-discord"
 __author__ = "Wiktor Jaworski"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023, 2024 Wiktor Jaworski"
-__version__ = "0.8.4"
+__version__ = "0.9.0"
 
 from . import console, errors, utils
 from .sggw_bot import SGGWBot
